b69216effb0b14c83258e8a7f9469f7a00cf5641
Eliseu dos Santos <s.eliseu.santos@gmail.com>
2015-11-03 07:58:30 -0200

    Correção na chamada e inserção de comentários
    Corrigir a chamada das funções dentro do prototype.init pois o this da
    função estava dando conflito com o this do addEventListener.Para
    corrigir foi adicionado uma variável _ths que recebe o this da  função
    logo acima.

--- CopyPaste.js
+++ CopyPaste.js
@@ -1,5 +1,18 @@
+/** 
+  * @ plugin CopyPaste fornece uma maneira fácil de copiar texto para a área de transferência
+  * @Autor Eliseu dos Santos | s.eliseu.santos@gmail.com | https://github.com/EliseuSantos
+  * Modo de Uso: 
+  * var copypaste = new CopyPaste('#trigger','.texto');
+  *@Parametros
+   O primeiro parâmetro é o botão ou elemento onde será dado o trigger para cópia.
+   O segundo parâmetro é o elemento(input,text-area ou tag) onde tem o texto a ser copiado.
+  *Opcional: Caso o elemento a ser copiado seja uma tag exemplo: <p> Texto a ser copiado </p>,
+    e o você deseja um conteudo mas específico basta adicionar um data-copy ao elemento sendo assim o plugin 
+    ignora seu value ou textontent pegando o valor que está no data-copy.
+*/
+
 'use strict';
-var elemento,trigger,data_atributo;
+var elemento,trigger,data_atributo,_this;
  
 function CopyPaste(trigger,elemento) {
   this.init(trigger,elemento);
@@ -7,20 +20,21 @@ function CopyPaste(trigger,elemento) {
  
 CopyPaste.prototype.init = function (trigger,elemento) {
   trigger = document.querySelector(trigger);
+  _this = this;
   trigger.addEventListener('click', function (){
     data_atributo = document.querySelector(elemento).getAttribute('data-copy');
     if (data_atributo != null) {
-      this.copiaTexto(data_atributo);
+      _this.copiaTexto(data_atributo);
     } else {
       var conteudo = document.querySelector(elemento);
-      if (this.verificaInputTextArea(conteudo)) {
+      if (_this.verificaInputTextArea(conteudo)) {
         if (conteudo.type === 'hidden') {
-          this.copiaTexto(conteudo.value);
+          _this.copiaTexto(conteudo.value);
         } else {
-          this.copiaInput(conteudo);
+          _this.copiaInput(conteudo);
         }
       } else {
-        this.copiaEstrutura(conteudo);
+        _this.copiaEstrutura(conteudo);
       }
     }
   });
@@ -36,8 +50,6 @@ CopyPaste.prototype.criaEstrutura = function (texto) {
 //Caso não seja input cria um range no texto e executa a cópia.
 CopyPaste.prototype.copiaEstrutura = function (texto) {
   var selecao = getSelection();
-  selecao.removeAllRanges();
-
   var range = document.createRange();
   range.selectNodeContents(texto);
   selecao.addRange(range);
@@ -65,4 +77,3 @@ CopyPaste.prototype.copiaTexto = function(texto) {
   this.copiaEstrutura(tag);
   document.body.removeChild(tag);
 };
-
